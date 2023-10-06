22f6227aefd46ef490ffe3441af673f36d12ad66
Eric <ericmuthuipatch22@gmail.com>
2023-10-06 11:20:25 +0300

    commit

--- server/app.py
+++ server/app.py
@@ -13,7 +13,7 @@ from flask_jwt_extended import JWTManager, create_access_token, jwt_required, ge
 
 
 
-from models import CheckIn, db, User
+from models import CheckIn, db, User, Tm, Student
 
 
 
@@ -174,19 +174,6 @@ api.add_resource(CheckInByID, '/checkins/<int:id>')
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
 # Registration
 # Create a resource for User
 @app.route('/register', methods=['POST'])
@@ -243,14 +230,127 @@ def logout():
     return jsonify({"message": "Logged out successfully"}), 200
 
 
+
+# Route to get all Tm records
+@app.route("/tms", methods=['GET'])
+def get_tms():
+    tms = Tm.query.all()
+    return jsonify([tm.to_dict() for tm in tms])
+
+# Route to create a new Tm record
+@app.route("/add-tm", methods=['POST'])
+def create_tm():
+    data = request.get_json()
+    tm = Tm(Name=data.get('Name'), Phone=data.get('Phone'), Email=data.get('Email'))
+    db.session.add(tm)
+    db.session.commit()
+    return make_response(
+        jsonify({'id': tm.id, 'Name': tm.Name, 'Phone': tm.Phone, 'Email': tm.Email}),
+        201  # Use HTTP status code 201 for resource created
+    )
+
+# Route to update an existing Tm record by ID
+@app.route("/tms/<int:id>", methods=['PUT'])
+def update_tm(id):
+    data = request.get_json()
+    tm = Tm.query.get(id)
+
+    if not tm:
+        return make_response(jsonify({'error': 'Tm not found'}), 404)
+
+    # Update the Tm properties
+    if 'Name' in data:
+        tm.Name = data['Name']
+    if 'Phone' in data:
+        tm.Phone = data['Phone']
+    if 'Email' in data:
+        tm.Email = data['Email']
+
+    db.session.commit()
+
+    # Return the updated Tm record as a JSON response
+    return make_response(jsonify({'id': tm.id, 'Name': tm.Name, 'Phone': tm.Phone, 'Email': tm.Email}), 200)
+
+# Route to delete an existing Tm record by ID
+@app.route("/tms/<int:id>", methods=['DELETE'])
+def delete_tm(id):
+    tm = Tm.query.get(id)
+
+    if not tm:
+        return make_response(jsonify({'error': 'Tm not found'}), 404)
+
+    db.session.delete(tm)
+    db.session.commit()
+
+    return jsonify({'message': 'Tm deleted successfully'})
+
+# Students
+
+# Route to get all Student records
+@app.route("/students", methods=['GET'])
+def get_students():
+    students = Student.query.all()
+    return jsonify([student.to_dict() for student in students])
+
+# Route to create a new Student record
+@app.route("/add-student", methods=['POST'])
+def create_student():
+    data = request.get_json()
+    student = Student(Name=data.get('Name'), idnumber=data.get('idnumber'), phone=data.get('phone'), laptop_model=data.get('laptop_model'), serial_number=data.get('serial_number'), tm_name=data.get('tm_name'))
+    db.session.add(student)
+    db.session.commit()
+    return make_response(
+        jsonify({'id': student.id, 'Name': student.Name, 'idnumber': student.idnumber, 'phone': student.phone, 'laptop_model': student.laptop_model, 'serial_number': student.serial_number, 'tm_name': student.tm_name}),
+        201  # Use HTTP status code 201 for resource created
+    )
+
+# Route to update an existing Student record by ID
+@app.route("/students/<int:id>", methods=['PUT'])
+def update_student(id):
+    data = request.get_json()
+    student = Student.query.get(id)
+
+    if not student:
+        return make_response(jsonify({'error': 'Student not found'}), 404)
+
+    # Update the Student properties
+    if 'Name' in data:
+        student.Name = data['Name']
+    if 'idnumber' in data:
+        student.idnumber = data['idnumber']
+    if 'phone' in data:
+        student.phone = data['phone']
+    if 'laptop_model' in data:
+        student.laptop_model = data['laptop_model']
+    if 'serial_number' in data:
+        student.serial_number = data['serial_number']
+    if 'tm_name' in data:
+        student.tm_name = data['tm_name']
+
+    db.session.commit()
+
+    # Return the updated Student record as a JSON response
+    return make_response(jsonify({'id': student.id, 'Name': student.Name, 'idnumber': student.idnumber, 'phone': student.phone, 'laptop_model': student.laptop_model, 'serial_number': student.serial_number, 'tm_name': student.tm_name}), 200)
+
+# Route to delete an existing Student record by ID
+@app.route("/students/<int:id>", methods=['DELETE'])
+def delete_student(id):
+    student = Student.query.get(id)
+
+    if not student:
+        return make_response(jsonify({'error': 'Student not found'}), 404)
+
+    db.session.delete(student)
+    db.session.commit()
+
+    return jsonify({'message': 'Student deleted successfully'})
+
+
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
 
     
     
-    
-    
-    
-    
+
     
    
--- server/models.py
+++ server/models.py
@@ -1,4 +1,3 @@
-from flask import Flask, jsonify, request, make_response
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy_serializer import SerializerMixin
 from datetime import datetime
@@ -32,3 +31,28 @@ class User(db.Model, SerializerMixin):
 
     def __repr__(self):
         return f'User(ID: {self.id}, Name: {self.name}, Email: {self.email}, Phone: {self.phone}, ID Number: {self.id_number})'
+
+
+class Tm(db.Model, SerializerMixin):
+    __tablename__ = 'tm'
+    id = db.Column(db.Integer, primary_key=True)
+    Name = db.Column(db.String)
+    Phone = db.Column(db.String)
+    Email = db.Column(db.String)
+
+    def __repr__(self):
+        return f'Tm: {self.Name}, Phone: {self.Phone}, Email: {self.Email}'
+    
+
+class Student(db.Model, SerializerMixin):
+    __tablename__ = 'student'
+    id = db.Column(db.Integer, primary_key=True)
+    Name = db.Column(db.String)
+    idnumber = db.Column(db.String)  
+    phone = db.Column(db.String)  
+    laptop_model = db.Column(db.String)  
+    serial_number = db.Column(db.String)  
+    tm_name = db.Column(db.String)  
+
+    def __repr__(self):
+        return f'Student: {self.Name}, ID: {self.idnumber}, Phone: {self.phone}, Laptop Model: {self.laptop_model}, Serial Number: {self.serial_number}, TM: {self.tm_name}'
